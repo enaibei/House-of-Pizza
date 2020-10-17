c34cb138de363191ac58eaa205651d986304ca10
Esther.Naibei <esthernaibei2020@gmail.com>
2020-10-17 21:28:53 +0300

    add prices for pizza

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,33 @@ function Getpizza( name,size,crust,topping, total ){
   this.topping = topping;
   this.total = total;
 }
+
+$(document).ready(function(){
+    $("button.proceed").click(function(event){
+     let pname = $(".name option:selected").val();
+     let psize = $("#size option:selected").val();
+     let pcrust = $("#crust option:selected").val();
+     let ptopping = [];
+     $.each($("input[name='toppings']:checked"), function(){            
+         ptopping.push($(this).val());
+     });
+     console.log(ptopping.join(", "));
+  
+     switch(psize){
+      case "0":
+        price =0;
+      break;
+      case "large":
+         price = 1200;
+         console.log(price);
+       break;
+       case "medium":
+         price = 850;
+         console.log("The price is "+price);
+       break;
+       case "small":
+         price = 600;
+         console.log(price);
+       default:
+         console.log("error"); 
+     }
