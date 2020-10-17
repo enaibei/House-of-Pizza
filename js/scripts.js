c6fe808397aba4e38fac923800feca7bd4f99568
Esther.Naibei <esthernaibei2020@gmail.com>
2020-10-17 21:57:11 +0300

    add functionality to home delivery option

--- js/scripts.js
+++ js/scripts.js
@@ -147,4 +147,17 @@ $(document).ready(function(){
         console.log("Your total bills is sh. "+checkoutTotal);
         $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
       });
+
+      // home delivery button
+    $("button.deliver").click(function(){
+        $(".pizzatable").hide();
+        $(".choise h2").hide();
+        $(".delivery").slideDown(1000);
+        $("#addedprice").hide();
+        $("button.deliver").hide();
+        $("#pizzatotal").hide();
+        let deliceryamount= checkoutTotal+150;
+        console.log("You will pay sh. "+deliceryamount+" on delivery");
+        $("#totalbill").append("Your bill plus delivery fee is: "+deliceryamount);
+      });
   
